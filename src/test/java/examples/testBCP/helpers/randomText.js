702f557f9950b850541cff7ae3e8d95958cd36d6
coostadam <149526445+coostadam@users.noreply.github.com>
2025-08-07 14:27:21 +0200

    borrados archivos innecesarios

--- src/test/java/examples/testBCP/helpers/randomText.js
+++
@@ -1,9 +0,0 @@
-function(min, max){
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    var len = Math.floor(Math.random() * (max - min + 1)) + min;
-    var result = '';
-    for (var i = 0; i < len; i++) {
-      result += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return result.trim();
-}
